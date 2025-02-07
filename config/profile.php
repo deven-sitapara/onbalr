f95096ea1cdccfacaff13b3a3243034c34e0065d
Deven S <shriramsoft@gmail.com>
2025-02-07 20:56:29 +0530

    initial commit

---
+++ config/profile.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'fields' => [
+        // 'organization' => [
+        //     'label' => 'Organization',
+        //     'type' => 'TextInput',
+        //     'rules' => 'required'
+        // ],
+        // 'sport' => [
+        //     'label' => 'Sport',
+        //     'type' => 'TextInput',
+        //     'rules' => 'required'
+        // ],
+        'about' => [
+            'label' => 'About',
+            'type' => 'Textarea',
+            'rules' => 'required'
+        ],
+        'occupation' => [
+            'label' => 'What do you do for a living?',
+            'type' => 'TextInput',
+            'rules' => ''
+        ]
+    ],
+];
