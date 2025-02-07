6730b67885fcff966fa35731a0aee3cd08b9d593
Deven S <shriramsoft@gmail.com>
2025-02-07 21:29:43 +0530

    profile and admin updated

--- config/profile.php
+++ config/profile.php
@@ -2,16 +2,39 @@
 
 return [
     'fields' => [
-        // 'organization' => [
-        //     'label' => 'Organization',
-        //     'type' => 'TextInput',
-        //     'rules' => 'required'
-        // ],
-        // 'sport' => [
-        //     'label' => 'Sport',
-        //     'type' => 'TextInput',
-        //     'rules' => 'required'
-        // ],
+        'logo' => [
+            'label' => 'Logo',
+            'type' => 'FileUpload',
+            'rules' => 'required'
+        ],
+        'organization' => [
+            'label' => 'Organization',
+            'type' => 'TextInput',
+            'rules' => 'required'
+        ],
+        'sport' => [
+            'label' => 'Sport',
+            'type' => 'TextInput',
+            'rules' => 'required'
+        ],
+        'primary_color' => [
+            'label' => 'Primary Color',
+            'type' => 'ColorPicker',
+            'rules' => 'required'
+        ],
+        'country' => [
+            'label' => 'County',
+            'type' => 'Select',
+            'rules' => 'required',
+            'options' => [
+                // us ,canada, other
+                'us' => 'United States',
+                'ca' => 'Canada',
+                'other' => 'Other'
+
+
+            ]
+        ],
         'about' => [
             'label' => 'About',
             'type' => 'Textarea',
