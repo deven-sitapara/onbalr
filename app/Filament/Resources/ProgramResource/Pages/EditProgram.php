175ba915a286cc3490ce2b6d0bdb51eacc471a18
Deven S <shriramsoft@gmail.com>
2025-02-11 22:58:41 +0530

    redirect after form submit

--- app/Filament/Resources/ProgramResource/Pages/EditProgram.php
+++ app/Filament/Resources/ProgramResource/Pages/EditProgram.php
@@ -3,17 +3,14 @@
 namespace App\Filament\Resources\ProgramResource\Pages;
 
 use App\Filament\Resources\ProgramResource;
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProgram extends EditRecord
 {
     protected static string $resource = ProgramResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return $this->getResource()::getUrl('index');
     }
 }
